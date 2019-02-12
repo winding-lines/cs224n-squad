f0257e3d8bf29866d548f6ae53ab86ed76e950b9
Marius Seritan <mseritan@linkedin.com>
2019-02-11 21:24:35 -0800

    Add pytorch-pretrained-bert

---
+++ run-bert.sh
@@ -0,0 +1,4 @@
+python bert/train.py  --bert_model bert-base-uncased --output_dir bert_output \
+    --do_predict --predict_file data/dev-v2.0.json \
+    --do_train --train_file data/train-v2.0.json --num_train_epochs 1.0 \
+    --version_2_with_negative 
