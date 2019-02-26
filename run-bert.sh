f4df678e1e49c9c26a404a7de0399045d97bf7b4
Ubuntu <mseritan@gpu1.u04k0kf2b5hetnic0wpn0cekqc.bx.internal.cloudapp.net>
2019-02-26 03:32:14 +0000

    bert changes on azure

--- run-bert.sh
+++ run-bert.sh
@@ -1,4 +1,5 @@
 python bert/train.py  --bert_model bert-base-uncased --output_dir bert_output \
     --do_predict --predict_file data/dev-v2.0.json \
     --do_train --train_file data/train-v2.0.json --num_train_epochs 1.0 \
-    --version_2_with_negative 
+    --max_seq_length 64 --train_batch_size 64 \
+    --version_2_with_negative
