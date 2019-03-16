76998e0fbfa214efb9a04d3fe4ff878219dddeab
Marius Seritan <mseritan@linkedin.com>
2019-03-16 08:23:10 -0700

    First attempt at fuse eq (8) and (9)

--- slqa.py
+++ slqa.py
@@ -112,31 +112,31 @@ class AlignedAttention(nn.Module):
     def __init__(self, hidden_size, drop_prob=0.1):
         super(AlignedAttention, self).__init__()
         self.drop_prob = drop_prob
-        self.c_weight = nn.Parameter(torch.zeros(hidden_size, 1))
+        self.p_weight = nn.Parameter(torch.zeros(hidden_size, 1))
         self.q_weight = nn.Parameter(torch.zeros(hidden_size, 1))
         self.cq_weight = nn.Parameter(torch.zeros(1, 1, hidden_size))
-        for weight in (self.c_weight, self.q_weight, self.cq_weight):
+        for weight in (self.p_weight, self.q_weight, self.cq_weight):
             nn.init.xavier_uniform_(weight)
         self.bias = nn.Parameter(torch.zeros(1))
 
-    def forward(self, c, q, c_mask, q_mask):
-        batch_size, c_len, _ = c.size()
+    def forward(self, p, q, p_mask, q_mask):
+        batch_size, p_len, _ = p.size()
         q_len = q.size(1)
-        s = self.get_similarity_matrix(c, q)        # (batch_size, c_len, q_len)
-        c_mask = c_mask.view(batch_size, c_len, 1)  # (batch_size, c_len, 1)
+        s = self.get_similarity_matrix(p, q)        # (batch_size, p_len, q_len)
+        p_mask = p_mask.view(batch_size, p_len, 1)  # (batch_size, p_len, 1)
         q_mask = q_mask.view(batch_size, 1, q_len)  # (batch_size, 1, q_len)
         
         # approx eq(4)
-        s1 = masked_softmax(s, q_mask, dim=2)       # (batch_size, c_len, q_len)
+        s1 = masked_softmax(s, q_mask, dim=2)       # (batch_size, p_len, q_len)
 
         # approx eq(5)
-        # (bs, c_len, q_len) x (bs, q_len, hid_size) => (bs, c_len, hid_size)
+        # (bs, p_len, q_len) x (bs, q_len, hid_size) => (bs, p_len, hid_size)
         p_tilde = torch.bmm(s1, q)
 
         # approx eq(6)
-        s2 = masked_softmax(s, c_mask, dim=1)       # (batch_size, c_len, q_len)
+        s2 = masked_softmax(s, p_mask, dim=1)       # (batch_size, p_len, q_len)
         # approx eq(7)
-        q_tilde = torch.bmm(s2.transpose(1,2), c) # (bs, q_len, hid_size)
+        q_tilde = torch.bmm(s2.transpose(1,2), p) # (bs, q_len, hid_size)
 
         x = (p_tilde, q_tilde)  
 
@@ -153,14 +153,14 @@ class AlignedAttention(nn.Module):
         See Also:
             Equation 1 in https://arxiv.org/abs/1611.01603
         """
-        c_len, q_len = c.size(1), q.size(1)
-        c = F.dropout(c, self.drop_prob, self.training)  # (bs, c_len, hid_size)
+        p_len, q_len = c.size(1), q.size(1)
+        c = F.dropout(c, self.drop_prob, self.training)  # (bs, p_len, hid_size)
         q = F.dropout(q, self.drop_prob, self.training)  # (bs, q_len, hid_size)
 
-        # Shapes: (batch_size, c_len, q_len)
-        s0 = torch.matmul(c, self.c_weight).expand([-1, -1, q_len])
+        # Shapes: (batch_size, p_len, q_len)
+        s0 = torch.matmul(c, self.p_weight).expand([-1, -1, q_len])
         s1 = torch.matmul(q, self.q_weight).transpose(1, 2)\
-                                           .expand([-1, c_len, -1])
+                                           .expand([-1, p_len, -1])
         s2 = torch.matmul(c * self.cq_weight, q.transpose(1, 2))
         s = s0 + s1 + s2 + self.bias
 
@@ -191,64 +191,59 @@ class SLQA(nn.Module):
         self.att = AlignedAttention(hidden_size=2 * hidden_size,
                                          drop_prob=drop_prob)
 
+        self.p_fusion = Fusion(2*hidden_size, 2*hidden_size)
+
+        self.q_fusion = Fusion(2*hidden_size, 2*hidden_size)
+
         self.q_self_align_final = SelfAlign(2 * hidden_size)
         self.p_temp_hack = SelfAlign(2 * hidden_size)
 
-        """
-        BiDAF
-
-        self.mod = layers.RNNEncoder(input_size=8 * hidden_size,
-                                     hidden_size=hidden_size,
-                                     num_layers=2,
-                                     drop_prob=drop_prob)
-
-        self.out = layers.BiDAFOutput(hidden_size=hidden_size,
-                                      drop_prob=drop_prob)
-        """
         self.bilinear_start = BilinearSeqAtt(2*hidden_size, 2*hidden_size)
         self.bilinear_end = BilinearSeqAtt(2*hidden_size, 2*hidden_size)
 
-    def forward(self, cw_idxs: torch.Tensor, cc_idxs: Optional[torch.Tensor], qw_idxs: torch.Tensor, qc_idxs: Optional[torch.Tensor]):
+    def forward(self, pw_idxs: torch.Tensor, pc_idxs: Optional[torch.Tensor], qw_idxs: torch.Tensor, qc_idxs: Optional[torch.Tensor]):
         """ Run a forward step
-            cw_idxs: word indices in the context  64, 254
-            cc_idxs: char indices in the context  64, 254, 16
+            pw_idxs: word indices in the paragraph  64, 254
+            pc_idxs: char indices in the paragraph  64, 254, 16
             qw_idxs: word indices in the question 64, 20
             qc_idx: char indices in the question  64, 20, 16
         """
 
-        c_mask = torch.zeros_like(cw_idxs) != cw_idxs
+        p_mask = torch.zeros_like(pw_idxs) != pw_idxs
         q_mask = torch.zeros_like(qw_idxs) != qw_idxs
-        c_len, q_len = c_mask.sum(-1), q_mask.sum(-1)
+        p_len, q_len = p_mask.sum(-1), q_mask.sum(-1)
 
-        c_emb = self.emb(cw_idxs, cc_idxs) # (batch_size, c_len, hidden_size)
+        p_emb = self.emb(pw_idxs, pc_idxs) # (batch_size, p_len, hidden_size)
         q_emb = self.emb(qw_idxs, qc_idxs) # (batch_size, q_len, hidden_size)
 
         # eq (1)
-        c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
+        p_enc = self.enc(p_emb, p_len)    # (batch_size, p_len, 2 * hidden_size)
 
         # approx eq (2)
         q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
 
         # approx eq (3)-(7)
-        (p_tilde, q_tilde) = self.att(c_enc, q_enc,
-                       c_mask, q_mask)    # 2 x tensors (batch_size, c_len, 2*hidden_size) 
+        (p_tilde, q_tilde) = self.att(p_enc, q_enc,
+                       p_mask, q_mask)    # 2 x tensors (batch_size, p_len, 2*hidden_size) 
+
+        # eq (8)
+        p_fused = self.p_fusion(p_enc, p_tilde)
+
+        # eq (9)
+        q_fused = self.q_fusion(q_enc, q_tilde)
 
         # paragraph partial processing
-        contextual_p = self.p_temp_hack(p_tilde)
+        contextual_p = p_fused# self.p_temp_hack(p_tilde)
+        # print(f"input {p_tilde.shape} output {contextual_p.shape}")
 
         # question partial processing        
         # eq (19)
         weighted_q = self.q_self_align_final(q_tilde)
 
-        """
-        mod = self.mod(att, c_len)        # (batch_size, c_len, 2 * hidden_size) 64, 54, 200
-
-        out = self.out(att, mod, c_mask)  # 2 tensors, each (batch_size, c_len)
-        """
-        logits_start = self.bilinear_start(weighted_q, p_tilde)
-        logits_end = self.bilinear_end(weighted_q, p_tilde)        
-        log_start = masked_softmax(logits_start, c_mask, log_softmax=True)
-        log_end = masked_softmax(logits_end, c_mask, log_softmax=True)
+        logits_start = self.bilinear_start(weighted_q, contextual_p)
+        logits_end = self.bilinear_end(weighted_q, contextual_p)        
+        log_start = masked_softmax(logits_start, p_mask, log_softmax=True)
+        log_end = masked_softmax(logits_end, p_mask, log_softmax=True)
 
         out = (log_start, log_end)
         return out
